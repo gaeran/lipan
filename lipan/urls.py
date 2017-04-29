9bea6fdd1dd9632471fb721723a51772a465834e
gaeran <gaeran@gmail.com>
2017-04-29 10:22:50 -0500

    20170429 1021
    Constructed project landing page and added additional bootstrap templating to project.

--- lipan/urls.py
+++ lipan/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    ##url(r'^$', include('888.urls')),
+    url(r'^$', TemplateView.as_view(template_name='static_pages/index.html'),
+        name='home'),
+    #url(r'^$', include('888.urls')),
     url(r'^portscan/', include('portscan.urls')),
 ]
