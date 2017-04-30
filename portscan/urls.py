ef8c9fbbddd839013ade2ab60890bbc9435d508a
gaeran <gaeran@gmail.com>
2017-04-30 08:22:36 -0500

    20170430 0821 - urls error correction

--- portscan/urls.py
+++ portscan/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^register/', views.register, name='register'),
     url(r'^(?P<ScanResults_id>[0-9]+)/$', views.detail, name='detail'),
+    #url(r'^(?P<pk>[0-9]+)/', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results$', views.ResultsView.as_view(), name='results'),
     url(r'scan/add/', views.ScanResultsCreate.as_view(), name='scan-create'),
     url(r'^addapp/$',TemplateView.as_view(template_name='portscan/addapp.html'), name='addapp'),
-    
 ]
